b2f0203cfe802399f2eb317131d5aad127dd547c
bhautik <rrr237623@gmail.com>
2026-04-17 17:28:16 +0530

    change name

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ec2-cicd-app');
+  protected readonly title = signal('ec2-cicd-app by bhautik');
 }
