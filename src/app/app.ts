4c64b7eddc96cd7e7d7ecbbdaafa11b0f55faadf
bhautik <rrr237623@gmail.com>
2026-04-17 18:32:20 +0530

    change of anguler json file

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ec2-cicd-app by bhautik');
+  protected readonly title = signal('launch by bhautik');
 }
